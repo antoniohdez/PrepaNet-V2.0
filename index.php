20136868509dac8d41775fd58c5000754d86c20e
Antonio <antonio.hdez93@gmail.com>
2013-10-18 16:55:55 -0500

    Primer commit del proyecto
    Base de plantilla html con toolbar responsivo utilizando bootstrap.

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<title>
+        PrepaNet - Inscripciones
+    </title>
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="js/html5shiv.js"></script>
+		<script src="js/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.php">Prepanet</a>
+			</div>
+			<div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+					<li class="active"><a href="#">Inicio</a></li>
+				</ul>            
+                <ul class="nav navbar-nav navbar-right">
+  					<li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Usuario<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Cerrar Sesión</a></li>
+                        </ul>
+                    </li>
+				</ul> 
+			</div><!--/.nav-collapse -->
+		</div>
+	</div><!--/.navbar -->
+    
+    <div>
+    </div>
+	
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
+</body>
+</html>
